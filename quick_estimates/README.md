3be5c7151f931256f8bf5db798f811ee2508ba13
Tyrel Hiebert <tyrelh@uvic.ca>
2018-08-06 17:28:50 -0700

    Add Quick Estimates solution

---
+++ quick_estimates/README.md
@@ -0,0 +1,2 @@
+# Quick Estimate Problem Link
+[https://open.kattis.com/problems/quickestimate](https://open.kattis.com/problems/quickestimate)
---
+++ quick_estimates/quick_estimates.py
@@ -0,0 +1,7 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+
+for _ in range(n):
+    val = sys.stdin.readline().rstrip()
+    print(len(val))
