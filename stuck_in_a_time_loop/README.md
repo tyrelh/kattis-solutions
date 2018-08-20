ecb6b3510be2bd81c055023d5e3df530bb51eda6
Tyrel Hiebert <tyrelh@uvic.ca>
2018-08-19 18:21:03 -0700

    Add Stuck in a Time Loop solution

---
+++ stuck_in_a_time_loop/README.md
@@ -0,0 +1,2 @@
+# Stuck in a Time Loop Problem Link
+[https://open.kattis.com/problems/timeloop](https://open.kattis.com/problems/timeloop).
---
+++ stuck_in_a_time_loop/siatl.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class siatl {
+    public static void main (String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        for(int i = 1; i <= num; i++) {
+            System.out.println(i + " Abracadabra");
+        }
+    }
+}
