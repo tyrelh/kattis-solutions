ec42f0a61fe21e6b22f07a63fd2ff7a931b08be5
Tyrel Hiebert <tyrelh@uvic.ca>
2018-10-18 08:00:09 -0700

    Add Solving for Carrots solution

---
+++ solving_for_carrots/README.md
@@ -0,0 +1,2 @@
+# Solving for Carrots Problem Link
+[https://open.kattis.com/problems/carrots](https://open.kattis.com/problems/carrots).
---
+++ solving_for_carrots/sfc.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class sfc {
+    public static void main (String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        num = in.nextInt();
+        System.out.print("" + num);
+    }
+}
